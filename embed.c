c308580762f050e81f79dfa598526f0a18ae6403
Hayato Hashimoto (橋本 隼人) <hayato.hashimoto@gmail.com>
2011-06-23 22:03:25 +0900

    implement closures though incomplete

--- embed.c
+++ embed.c
@@ -1,15 +1,17 @@
 #include <chicken.h>
 #include <assert.h>
 #include <malloc.h>
+#include <stdio.h>
 #include <sys/mman.h>
 #include <readline/readline.h>
+#include <fcntl.h>
 
 long long int service (long long int num, long long int arg) {
  printf ("hook\n");
  return 0;
 }
 
-int main() {
+int main (int argc, char** argv) {
   char str[1024], *text, *rtext;
   C_word *result;
   result = malloc(1024);
@@ -19,18 +21,15 @@ int main() {
   C_word *data[1];
   data[0] = &val;
   C_word k = CHICKEN_run (C_toplevel);
-  char* c   = mmap (0, 4096, PROT_WRITE|PROT_EXEC|PROT_READ,MAP_PRIVATE|MAP_ANON,-1,0); 
-  char* mem = mmap (0, 4096, PROT_WRITE|PROT_READ,MAP_PRIVATE|MAP_ANON,-1,0);
+  int fd    = open (argv[1], O_RDWR);
+  char* c   = (fd != -1) ? mmap (0, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, fd, 0)
+                         : mmap (0, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, fd, 0);
+  char* mem = mmap (0, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
   printf ("code will be loaded at 0x%Lx\n", c);
  while (1) {
-  text = readline("test> ");
-  sprintf (str, "(opcode-compile '(lambda () %s) #x%Lx)", text, c);
-  status = CHICKEN_eval_string_to_string (str, rtext, 1024);
-  assert (status);
-  sprintf (str, "(binary-compile '(lambda () %s) #x%Lx)", text, c);
+  sprintf (str, "(read-compile #x%Lx)", c);
   status = CHICKEN_eval_string (str, result);
   assert (status);
-  rtext  = C_c_string(result);
   int i, j;
   int len = *((int *) *result);
   char* binary = ((char *) *result) + 8;
@@ -40,7 +39,7 @@ int main() {
     *(c+i) = *(binary+i);
     printf ("%02hhx ", *(c+i));
     i++;}
-   printf ("\n", c+i); 
+   printf ("\n"); 
   }
   long long int v = ((long long int (*) (char*, int, long long int (*) (long long int, long long int))) c)  (mem, 4096, service);
   printf (" => %d (0x%Lx)\n", v, v);
